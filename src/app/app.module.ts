2901dca02385890af0da25454f1d1ad2c4c4b5cf
Kamesh <kameshburde35@gmail.com>
2020-04-14 23:39:17 +0530

    login page home page and corousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
-import {CarouselModule} from 'primeng/carousel';
-//import { CorouselComponentComponent } from './components/corousel/corousel-component/corousel-component.component';
 import { CorouselComponent } from './components/corousel/corousel.component';
+import { RouterModule, Routes } from '@angular/router';
+import _ from 'lodash';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+import { FormsModule } from '@angular/forms';
+import { MbscModule } from '@mobiscroll/angular-lite';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CorouselComponent
+    CorouselComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    CartPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CarouselModule
+    RouterModule,
+    CarouselModule,
+    FormsModule,
+    MbscModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
